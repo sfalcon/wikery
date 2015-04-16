b74f641580f311347f5f3df955727b2a0303accd
Samuel Falcon <samuel.falcon.fdez@gmail.com>
2015-04-16 12:35:24 +0100

    choosing xml parser

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject wikery "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description
+  "System that reads a wiki format file of abstracts and allows querying"
+  :url ""
+  :license {:name "CDDL-1.0"
+            :url "http://opensource.org/licenses/CDDL-1.0"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.xml "0.0.9"]]
+  :plugins [[lein-marginalia "0.8.0"]]
+  :aliases {"autotest" ["midje" ":autotest"]}
   :main ^:skip-aot wikery.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]}})
--- test/wikery/core_test.clj
+++ test/wikery/core_test.clj
@@ -1,8 +1,11 @@
 (ns wikery.core-test
   (:use midje.sweet
         clojure.java.io)
-  (:require [wikery.core :refer :all]))
+  (:require [wikery.core    :refer :all]
+            [wikery.parsing :refer :all]))
 
 (fact "wikipedia sample abstracts file is in place"
       (.exists
        (as-file "resources/enwiki-latest-abstract23.xml")) => true)
+
+(fact "")
