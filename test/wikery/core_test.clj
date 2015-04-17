fedfe467b4080c887255572420ba2b5f1fd99f97
Samuel Falcon <samuel.falcon.fdez@gmail.com>
2015-04-17 14:05:00 +0100

    refactor

--- test/wikery/core_test.clj
+++ test/wikery/core_test.clj
@@ -1,13 +1,11 @@
 (ns wikery.core-test
   (:use midje.sweet
-        clojure.java.io)
-  (:require [wikery.core    :refer :all]
-            [wikery.parsing :refer :all]))
+        wikery.core))
 
-(fact "wikipedia sample abstracts file is in place"
-      (.exists
-       (as-file "resources/enwiki-latest-abstract23.xml")) => true)
+
 
 (facts "about parsing the abstracts file"
+       (fact "wikipedia sample abstracts file is in place"
+             true => (.exists (as-file "resources/enwiki-latest-abstract23.xml")))
        (fact "the first title of the file is -> Wikipedia: Ahmad Reza Pourdastan"
              (query :title :pos 1) => "Wikipedia: Ahmad Reza Pourdastan"))
