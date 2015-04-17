d9ab2e1fe560338c3a0be09eea1bcf2bbac4defe
Samuel Falcon <samuel.falcon.fdez@gmail.com>
2015-04-17 19:26:26 +0100

    query

--- test/wikery/core_test.clj
+++ test/wikery/core_test.clj
@@ -1,11 +1,17 @@
 (ns wikery.core-test
-  (:use midje.sweet
+  (:use clojure.java.io
+        midje.sweet
         wikery.core))
 
-
-
-(facts "about parsing the abstracts file"
+(facts "about the abstracts file"
        (fact "wikipedia sample abstracts file is in place"
-             true => (.exists (as-file "resources/enwiki-latest-abstract23.xml")))
+             true => (.exists (as-file "resources/enwiki-latest-abstract23.xml"))))
+
+(facts "about querying the parsed file"
        (fact "the first title of the file is -> Wikipedia: Ahmad Reza Pourdastan"
-             (query :title :pos 1) => "Wikipedia: Ahmad Reza Pourdastan"))
+             (query :title :pos 1) => "Wikipedia: Ahmad Reza Pourdastan")
+       (fact "the title 10 of the file is ->  Wikipedia: Charles Lennox Wyke"
+             (query :title :pos 10) => "Wikipedia: Charles Lennox Wyke"))
+
+(facts "about reformatting the parsed file"
+       )
